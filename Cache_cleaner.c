1ef0a3972a562aac00c7fa24b0725f5eec25bd3e
MZain3028 <72807201+MZain3028@users.noreply.github.com>
2023-05-10 13:28:58 +0500

    Create Cache_cleaner.c
    Initial main menu

---
+++ Cache_cleaner.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+//Function prototypes
+bool SystemCacheMemory();
+bool ClearOutSwap();
+bool DNSCscheMemory();
+bool TemporaryFile();
+bool RAMCacheMemory();
+bool ARPCacheMemory();
+bool OtherCache();
+bool DoAll();
+
+//Main function
+int main()
+{
+	int choice;
+	int check = 0;
+	bool result;
+	while(check == 0)
+	{
+		printf("Select which memory do you need to be erased\n");
+		printf("1. System cache memory.\n2. Clear out Swap\n3. DNS cach Memory\n4. temporary Files\n5. RAM cache Memory\n6. ARP cache Memory\n7. Clearing other caches on Ubuntu\n8. Clean All\n0. Exit\n");
+		printf("Enter your choice: ");
+		scanf("%d",&choice);
+		
+		if(choice == 0)
+		{
+			check++;
+			return 0;
+		}
+		
+		switch(choice)
+		{
+			case 1:
+				result = SystemCacheMemory();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 2:
+				result = ClearOutSwap();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 3:
+				result = DNSCscheMemory();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 4:
+				result = TemporaryFile();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 5:
+				result = RAMCacheMemory();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 6:
+				result = ARPCacheMemory();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 7:
+				result = OtherCache();
+				if(result == 1)
+				{
+					printf("\n\nOperation status: true\n\n");
+				}
+				result = 0;
+				break;
+			case 8:
+				DoAll();
+				sleep(5);
+				break;
+			default:
+				printf("\nOut of range.\n");
+				break;
+		}
+		sleep(10);
+		system("clear");
+	}
+
+	return 0;
+}
